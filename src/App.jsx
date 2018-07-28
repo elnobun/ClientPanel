9a1df689ff34d7b253ef4808192f1dfed76f9a10
Ellis Enobun <elnobun@gmail.com>
2018-07-28 01:43:38 +0100

    route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppNavbar from './components/layout/AppNavbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Client Panel</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <p>Other links here</p>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
