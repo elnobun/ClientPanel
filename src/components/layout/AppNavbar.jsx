9c54d38b372bb438c4fd93c63461e7853ed2902f
Ellis Enobun <elnobun@gmail.com>
2018-07-28 01:43:23 +0100

    navbar component setup

---
+++ src/components/layout/AppNavbar.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { NavLink as Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+
+class AppNavbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            <i class="fas fa-database" />&nbsp; Client Panel Manager
+          </Link>
+          <button
+            type="button"
+            className="navbar-toggler"
+            data-toggle="collapse"
+            data-target="#navbarMain"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarMain">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/dashboard" className="nav-link">
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+AppNavbar.propTypes = {};
+
+export default AppNavbar;
